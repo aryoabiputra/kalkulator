73939ccc61b56bee0200214bf85e5699af8c1270
Aryo Abi Putra <aryoputra606@gmail.com>
2025-08-04 10:36:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function input(value){
+    document.getElementById("display").value += value;
+}
+
+function bersihkanLayar(){
+    document.getElementById("display").value ="";
+}
+
+function hasil(){
+    try{
+        const hasil = eval(document.getElementById("display").value);
+        document.getElementById("display").value = hasil;
+    }
+    catch{
+        alert("Maaf, ada kesalahan dalam perhitungan Anda");
+    }
+}
